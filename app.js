289458c40de402c59060947cacd17e126477bc17
Malek-Hamad <79150805+Malek-Hamad@users.noreply.github.com>
2022-03-17 12:34:55 +0100

    my webside using githup

---
+++ app.js
@@ -0,0 +1,10 @@
+const express =require("express");
+const app = express();
+const PORT = process.env.PORT || 5555;
+
+
+app.use(express.static("public"));
+app.use("/malek" , express.static("myWebside"));
+
+
+app.listen(PORT, () => {console.log("The server is lisning on port " + PORT)});
